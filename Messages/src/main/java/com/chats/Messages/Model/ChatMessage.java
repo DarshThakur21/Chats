bf9eb2583be7e19b6038d079cc7ac03e3e878f9e
Darsh Thakur <thakurdarsh21@gmail.com>
2025-05-04 02:26:19 +0530

    working version

--- Messages/src/main/java/com/chats/Messages/Model/ChatMessage.java
+++ Messages/src/main/java/com/chats/Messages/Model/ChatMessage.java
@@ -14,6 +14,54 @@ import java.util.Date;
 @Document
 public class ChatMessage {
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(String chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getRecipientId() {
+        return recipientId;
+    }
+
+    public void setRecipientId(String recipientId) {
+        this.recipientId = recipientId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
     @Id
     private String id;
     private String chatId;
--- Messages/src/main/java/com/chats/Messages/Model/ChatNotification.java
+++ Messages/src/main/java/com/chats/Messages/Model/ChatNotification.java
@@ -1,21 +1,18 @@
 package com.chats.Messages.Model;
 
-
 import lombok.*;
 import org.springframework.data.annotation.Id;
 
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Getter
+@Setter
 public class ChatNotification {
+
     @Id
     private String id;
     private String senderId;
     private String recipientId;
     private String content;
-
-
-
 }
